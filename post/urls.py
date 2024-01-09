c69733e4c95d2eea1345d334e693fb032249ea06
acemehadi <mehadihasan.rajib10@gmail.com>
2024-01-09 17:19:15 +0600

    Some changes

--- post/urls.py
+++ post/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
         path('create-post/', views.create_post, name="create_post"),
         path('<str:uuid>/', views.post_detail, name="post_detail"),
-         path('<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
+        path('<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
         path('<int:post_id>/like_post/', views.like_post, name='like_post'),
 ]
 
